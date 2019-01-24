8f51a9c05c6d425be2251982880edfb37101b7d5
adslin <46364113+adslin@users.noreply.github.com>
2019-01-24 15:18:21 -0800

    Update event.js

--- event.js
+++ event.js
@@ -1,38 +1,44 @@
-function changeText(obj) {
- obj.innerHTML = "Ooops! Detour ->->";
+//The click event occurs when the user clicks on an element
+document.addEventListener("click", changeText);
+function changeText() {  
+  document.getElementById("changeP").innerHTML = "Ooops! Detour ->->";
 }
 
-function mOver(obj) {
-  obj.innerHTML = "Hello World!";  
+//The over event occurs when the pointer is moved onto an element, or onto one of its children
+//The out event occurs when a user moves the mouse pointer out of an element, or out of one of its children
+document.addEventListener("mouseover", mOver);
+document.addEventListener("mouseout", mOut);
+function mOver() {
+  document.getElementById("moverP").innerHTML = "Hello World!";  
+}
+function mOut() {
+  document.getElementById("moverP").innerHTML = "Mouse Over Me";
 }
 
-function mOut(obj) {
-  obj.innerHTML = "Mouse Over Me";
+//The up event occurs when a user releases a mouse button over an element
+//The down event occurs when the user presses a mouse button over an element
+document.addEventListener("mouseup", mUp);
+document.addEventListener("mousedown", mDown);
+function mUp() {
+  document.getElementById("mP").style.backgroundColor = "blue";
+  document.getElementById("mP").innerHTML = "Thank You!";  
+}
+function mDown() {
+  document.getElementById("mP").style.backgroundColor = "yellow";
+  document.getElementById("mP").innerHTML = "Release Me";
 }
 
-function mUp(obj) {
-  obj.style.backgroundColor = "blue";
-  obj.innerHTML = "Thank You!";  
+//The double event occurs when the user double-clicks on an element
+document.addEventListener("dblclick", dbclickText);
+function dbclickText() { 
+  document.getElementById("dbP").style.color = "blue"; 
+  document.getElementById("dbP").innerHTML = "I was double-clicked!";
 }
 
-function mDown(obj) {
-  obj.style.backgroundColor="yellow";
-  obj.innerHTML = "Release Me";  
-}
-
-function upperCase() {
-  var x = document.getElementById("ucaseP");
-  x.addEventListener("onkeyup", upperCase);
-  x.value = x.value.toUpperCase();
-}
-
-function myFunction() {
-  document.getElementById("alertP").addEventListener("click", function() {
-	 	alert("You clicked the white element!");
-  }, false);
-  	
-  document.getElementById("alertDiv").addEventListener("click", function() {
-    alert("You clicked the lightblue element!");
-  }, false);
-
+//The onresize event occurs when the browser window has been resized
+window.addEventListener("resize", disSize);
+function disSize() { 
+   var w = window.innerWidth;
+   var h = window.innerHeight;
+   document.getElementById("sizeP").innerHTML = "Width: " + w + "<br>Height: " + h;
 }
